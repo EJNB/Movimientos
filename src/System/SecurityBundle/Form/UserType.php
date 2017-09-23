ba370c04e4c17681155e39c61ef89cc70437ebfa
Javier <javiernieto1989@gmail.com>
2017-09-23 12:41:49 -0400

    agregue el campo accupation en la tabla user

---
+++ src/System/SecurityBundle/Form/UserType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace System\SecurityBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('username')
+            ->add('password')
+            ->add('name');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'System\SecurityBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'system_securitybundle_user';
+    }
+
+
+}
